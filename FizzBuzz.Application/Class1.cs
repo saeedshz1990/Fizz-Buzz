22791880b87d5610170f06778732991f7bd4a1c3
Saeed Ansari <68070998+saeedshz1990@users.noreply.github.com>
2022-04-24 10:56:06 +0430

    Create FizzBuzz game

---
+++ FizzBuzz.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FizzBuzz.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ FizzBuzz.Logic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FizzBuzz.Logic
+{
+    public class Class1
+    {
+    }
+}
